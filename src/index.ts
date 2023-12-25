1320c9e625129bceb8767c403ea3a772d27469ad
RuadhanKM <9281239@students.k12.dc.us>
2023-12-25 15:51:24 -0500

    added ar.js sample code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express, { Express, Request, Response } from "express";
+import https from "https"
 
 const app: Express = express();
 const port = 3000;
+const address = process.env.ADDR || "localhost"
+
+var credentials = {key: process.env.SSL_PRIVATE_KEY?.replace(/\\n/gm, "\n"), cert: process.env.SSL_CERT?.replace(/\\n/gm, "\n")};
 
 app.use(express.static('src/public'))
+app.use(express.static('src/static'))
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+var httpsServer = https.createServer(credentials, app);
+
+httpsServer.listen(port, address, () => {
+    console.log(`Server is running at https://${address}:${port}`);
 });
