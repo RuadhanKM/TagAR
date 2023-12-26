e4f9e7d0c8fc87a4acebab1f38148941f8c729e5
RuadhanKM <9281239@students.k12.dc.us>
2023-12-25 19:26:17 -0500

    added server texture and client parsing

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,16 @@ const app: Express = express();
 const port = 3000;
 const address = process.env.ADDR || "localhost"
 
-var credentials = {key: process.env.SSL_PRIVATE_KEY?.replace(/\\n/gm, "\n"), cert: process.env.SSL_CERT?.replace(/\\n/gm, "\n")};
+const credentials = {key: process.env.SSL_PRIVATE_KEY?.replace(/\\n/gm, "\n"), cert: process.env.SSL_CERT?.replace(/\\n/gm, "\n")};
 
-app.use(express.static('src/public'))
+const textureMain = new Uint8ClampedArray(1024 * 1024).map(x => Math.random()*500)
+console.log(textureMain)
+
+app.use(express.static('dist/public'))
 app.use(express.static('src/static'))
+app.get("/texture-main", (req, res) => {
+    res.send(textureMain.join(" "))
+})
 
 var httpsServer = https.createServer(credentials, app);
 
---
+++ src/public/tag.ts
@@ -0,0 +1,22 @@
+async function load() {
+    const textureMain = new Uint8ClampedArray((await (await fetch("texture-main")).text()).split(' ').map(x => parseInt(x)))
+    let textureMainDisplayNode: HTMLImageElement | null = document.getElementById('tag-ar-texture-main') as HTMLImageElement
+    if (!textureMainDisplayNode) return console.error("Could not find texture display node!")
+
+    console.log(textureMain)
+
+    const textureMainImageData = new ImageData(textureMain, 1024)
+    let textureMainBlob
+
+    {
+        let bufferCanvas = new OffscreenCanvas(1024, 1024)
+        let bufferCtx = bufferCanvas.getContext("2d")
+        if (!bufferCtx) return console.error("Failed to create buffer canvas")
+        bufferCtx.putImageData(textureMainImageData, 0, 0)
+        textureMainBlob = await bufferCanvas.convertToBlob()
+    }
+
+    textureMainDisplayNode.src = URL.createObjectURL(textureMainBlob);
+}
+
+load()
