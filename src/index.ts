e0901eefd44f73fa59163cbe6c47f74cdcb3f42c
RuadhanKM <9281239@students.k12.dc.us>
2023-12-26 14:57:10 -0500

    localized aframe script, and increased color bit depth and resolution, and added compression

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from "express";
 import https from "https"
+import zlib from "zlib";
 
 const app: Express = express();
 const port = 3000;
@@ -7,13 +8,16 @@ const address = process.env.ADDR || "localhost"
 
 const credentials = {key: process.env.SSL_PRIVATE_KEY?.replace(/\\n/gm, "\n"), cert: process.env.SSL_CERT?.replace(/\\n/gm, "\n")};
 
-const textureMain = new Uint8ClampedArray(1024 * 1024).map(x => Math.random()*256)
-console.log(textureMain)
+const textureMain = Buffer.alloc(1024 * 1024 * 2).map(x => Math.random()*255 | 0b00_00_00_11)
 
 app.use(express.static('dist/public'))
 app.use(express.static('src/static'))
 app.get("/texture-main", (req, res) => {
-    res.send(textureMain.join(" "))
+    if (req.header('Accept-Encoding')?.split(', ').includes('gzip')) {
+        res.setHeader('Content-Encoding', 'gzip')
+        return res.send(zlib.gzipSync(textureMain))
+    }
+    res.send(textureMain)
 })
 
 var httpsServer = https.createServer(credentials, app);
--- src/public/tag.ts
+++ src/public/tag.ts
@@ -1,21 +1,34 @@
 async function load() {
-    const textureMain = new Uint8ClampedArray((await (await fetch("texture-main")).text()).split(' ').map(x => parseInt(x)))
+    const textureMainRaw = new Uint8ClampedArray(await (await fetch("texture-main")).arrayBuffer())
     let textureMainDisplayNode: HTMLImageElement | null = document.getElementById('tag-ar-texture-main') as HTMLImageElement
     if (!textureMainDisplayNode) return console.error("Could not find texture display node!")
 
+    let textureMain = new Uint8ClampedArray(textureMainRaw.length*2)
+
+    for (let i=0; i<textureMain.length; i+=4) {
+        let hiByteRep = textureMainRaw[i/2].toString(2).padStart(8, '0')
+        let loByteRep = textureMainRaw[i/2+1].toString(2).padStart(8, '0')
+
+        textureMain[i+0] = (parseInt(hiByteRep.slice(0, 2), 2) + parseInt(loByteRep.slice(0, 2), 2)) * 42.5
+        textureMain[i+1] = (parseInt(hiByteRep.slice(2, 4), 2) + parseInt(loByteRep.slice(2, 4), 2)) * 42.5
+        textureMain[i+2] = (parseInt(hiByteRep.slice(4, 6), 2) + parseInt(loByteRep.slice(4, 6), 2)) * 42.5
+        textureMain[i+3] = (parseInt(hiByteRep.slice(6   ), 2) + parseInt(loByteRep.slice(6   ), 2)) * 42.5     
+    }
+
     console.log(textureMain)
 
-    const textureMainImageData = new ImageData(textureMain, 256)
+    const textureMainImageData = new ImageData(textureMain, 1024)
     let textureMainBlob
 
     {
-        let bufferCanvas = new OffscreenCanvas(256, 256)
+        let bufferCanvas = new OffscreenCanvas(1024, 1024)
         let bufferCtx = bufferCanvas.getContext("2d")
         if (!bufferCtx) return console.error("Failed to create buffer canvas")
         bufferCtx.putImageData(textureMainImageData, 0, 0)
         textureMainBlob = await bufferCanvas.convertToBlob()
     }
 
+    
     textureMainDisplayNode.src = URL.createObjectURL(textureMainBlob);
 }
 
