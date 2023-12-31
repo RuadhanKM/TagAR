1f49a1093c738dd7ae08253229e06a0410cef3ae
RuadhanKM <9281239@students.k12.dc.us>
2023-12-31 07:58:28 -0500

    added texture writing

--- src/public/tag.ts
+++ src/public/tag.ts
@@ -1,13 +1,33 @@
 const arjs = require("@ar-js-org/ar.js")
+const three = require("@ar-js-org/ar.js/node_modules/three")
+
+let textureMainMaterial: { map: { needsUpdate: boolean; }; }
+let textureMainObj: THREE.Object3D
+
+function linePlaneIntersect(p0: THREE.Vector3, p1: THREE.Vector3, p_co: THREE.Vector3, p_no: THREE.Vector3): THREE.Vector3 | false {
+    let u = p1.clone().sub(p0)
+    let dot = p_no.dot(u)
+
+    if (Math.abs(dot) < 1e-6) return false
+
+    let w = p0.clone().sub(p_co)
+    let fac = -p_no.dot(w) / dot
+    u.multiplyScalar(fac)
+    return p0.clone().add(u)
+}
 
 AFRAME.registerComponent('canvas-updater', {
     dependencies: ['geometry', 'material'],
 
-    tick: function () {
-        var el = this.el;
-        var material = (el.getObject3D('mesh') as any).material;
+    init: function() {
+        let obj = (this.el.getObject3D('mesh') as any)
+        textureMainMaterial = obj.material;
+    }
+});
 
-        material.map.needsUpdate = true;
+AFRAME.registerComponent('get-position', {
+    init: function() {
+        textureMainObj = (this.el.object3D)
     }
 });
 
@@ -41,18 +61,42 @@ bufferCtx.putImageData(textureMainImageData, 0, 0)
 
 let interval: NodeJS.Timeout
 
-document.addEventListener('touchend', e => {
-    console.log("end")
-    clearInterval(interval)
-})
+document.addEventListener('contextmenu', e => e.preventDefault(), {passive: false})
 
+document.addEventListener('touchend', e => {
+    e.preventDefault()
+    clearInterval(interval)
+}, {passive: false})
+
+const lineZero = new three.Vector3(0,0,0)
+const lineOne = new three.Vector3(0,0,1) 
 document.addEventListener('touchstart', e => {
+    e.preventDefault()
     interval = setInterval(async () => {
-        console.log("touch")
         if (!bufferCtx) throw new Error("Failed to create rendering context!")
-        bufferCtx.fillRect(0, 0, 100, 100)
+
+        let position: THREE.Vector3 = new three.Vector3()
+        let normal: THREE.Vector3 = new three.Vector3()
+        
+        textureMainObj.getWorldPosition(position)
+        textureMainObj.getWorldDirection(normal)
+        normal = normal.applyAxisAngle(new three.Vector3(1,0,0), -Math.PI/2)
+
+        let planePoint = linePlaneIntersect(lineZero, lineOne, position, normal)
+        if (!planePoint) return
+
+        let worldScale = new three.Vector3()
+        textureMainObj.getWorldScale(worldScale)
+        let localPlanePoint = textureMainObj.worldToLocal(planePoint)
+
+        console.log((localPlanePoint.x+0.5)*1024, (-localPlanePoint.z+0.5)*1024)
+
+        bufferCtx.beginPath();
+        bufferCtx.arc((localPlanePoint.x+0.5)*1024, (localPlanePoint.z-0.5)*1024, 50, 0, 2 * Math.PI);
+        bufferCtx.fill();
+        textureMainMaterial.map.needsUpdate = true
     }, 50)
-})
+}, {passive: false})
 
 
 export {}
